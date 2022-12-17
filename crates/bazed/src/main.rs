454e98be902add5e90f2f4582e54ce83f6577dd3
elkowar <5300871+elkowar@users.noreply.github.com>
2022-12-17 17:57:47 +0100

    feat: Use tracing for logging

--- crates/bazed/src/main.rs
+++ crates/bazed/src/main.rs
@@ -1,7 +1,14 @@
 use color_eyre::Result;
+use tracing::Level;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
+    color_eyre::install()?;
+    tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .pretty()
+        .init();
+
     bazed_core::app::start("127.0.0.1:6969").await?;
 
     loop {
