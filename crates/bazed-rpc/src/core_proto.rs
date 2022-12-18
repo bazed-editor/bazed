df21bd1b4c8fb1d823ad96dda283454f697d7a4b
elkowar <5300871+elkowar@users.noreply.github.com>
2022-12-18 13:42:10 +0100

    feat(core): Implement vertical caret movement

--- crates/bazed-rpc/src/core_proto.rs
+++ crates/bazed-rpc/src/core_proto.rs
@@ -4,6 +4,13 @@ use uuid::Uuid;
 use crate::keycode::KeyInput;
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct CaretPosition {
+    line: usize,
+    column: usize,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case", tag = "method", content = "params")]
 pub enum ToFrontend {
     Open {
@@ -11,13 +18,10 @@ pub enum ToFrontend {
         title: String,
         text: String,
     },
-    UpdateText {
+    UpdateDocument {
         id: Uuid,
         text: String,
-    },
-    SetCursorPosition {
-        line: usize,
-        column: usize,
+        carets: Vec<CaretPosition>,
     },
 }
 
@@ -25,5 +29,5 @@ pub enum ToFrontend {
 #[serde(rename_all = "snake_case", tag = "method", content = "params")]
 pub enum ToBackend {
     KeyPressed(KeyInput),
-    MouseInput { line: usize, column: usize },
+    MouseInput(CaretPosition),
 }
