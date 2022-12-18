8c1e87a9ff87682c39152132f0c07da7f02653dc
elkowar <5300871+elkowar@users.noreply.github.com>
2022-12-18 16:07:40 +0100

    feat(core): Allow reading and writing files

--- crates/bazed-core/src/document.rs
+++ crates/bazed-core/src/document.rs
@@ -1,3 +1,5 @@
+use std::{io::Write, path::PathBuf};
+
 use bazed_rpc::core_proto::{CaretPosition, ToFrontend};
 use uuid::Uuid;
 
@@ -12,18 +14,39 @@ impl DocumentId {
 }
 
 pub struct Document {
-    pub title: String,
+    pub path: Option<PathBuf>,
     pub buffer: Buffer,
 }
 
 impl Document {
     pub fn open_ephemeral() -> Document {
         Self {
-            title: "<unnamed>".to_string(),
-            buffer: Buffer::open_ephemeral(),
+            path: None,
+            buffer: Buffer::new_empty(),
         }
     }
 
+    pub fn open_file(path: PathBuf) -> std::io::Result<Document> {
+        let content = std::fs::read_to_string(&path)?;
+        Ok(Self {
+            path: Some(path),
+            buffer: Buffer::new_from_string(content),
+        })
+    }
+
+    /// Save the current buffer state to its path. Does nothing when no path is set.
+    pub fn write_to_file(&self) -> std::io::Result<()> {
+        let Some(ref path) = self.path else { return Ok(()) };
+        let mut file = std::fs::File::options().write(true).open(path)?;
+        file.write_all(self.buffer.content_to_string().as_bytes())
+    }
+
+    /// Create a notification for the frontend, that contains all relevant state of this document.
+    ///
+    /// *Note:* This will later be replaced with a proper
+    /// damage tracking-style system that sends patches to the frontend.
+    /// Additionally, this will later only send updates concerning
+    /// the parts of the document that are currently visible / relevant in the frontend.
     pub fn create_update_notification(&self, id: DocumentId) -> ToFrontend {
         ToFrontend::UpdateDocument {
             id: id.0,
