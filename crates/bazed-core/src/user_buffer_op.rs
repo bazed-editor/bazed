dcefa50081fc915ed889f11868a94ce7e604a781
elkowar <5300871+elkowar@users.noreply.github.com>
2022-12-18 11:24:10 +0100

    feat(core): Add user_buffer_op module
    This represents any operations the user performs on a buffer

---
+++ crates/bazed-core/src/user_buffer_op.rs
@@ -0,0 +1,18 @@
+//! User-level operation that can be performed on a [Buffer].
+//! This includes edit and movement operations.
+//! These will occur at the caret positions, and are thus only used for directly userfacing operations
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub(crate) enum EditOp {
+    Insert(String),
+    Backspace,
+    Undo,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub(crate) enum MovementOp {
+    Left,
+    Right,
+    Up,
+    Down,
+}
