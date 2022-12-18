112cfbd145a67dca89eef8763b8ee3dd2825a982
elkowar <5300871+elkowar@users.noreply.github.com>
2022-12-18 14:01:39 +0100

    test(core): Add tests for basic insert, delete and move operations

--- crates/bazed-core/src/buffer.rs
+++ crates/bazed-core/src/buffer.rs
@@ -5,41 +5,41 @@ use tap::Pipe;
 use xi_rope::{engine::Engine, tree::NodeInfo, DeltaBuilder, Rope, RopeDelta, Transformer};
 
 use crate::{
-    region::{Region, RegionId},
+    mark::{Mark, MarkId},
     user_buffer_op::{BufferOp, EditOp, MovementOp},
 };
 
-/// Stores all the active regions in a buffer.
+/// Stores all the active marks in a buffer.
 ///
 /// Terminology:
-/// - *Cursor* refers to any region with length 0
-/// - *Caret* refers to cursors that represent concrete, user-controlled cursors.
+/// - *Mark* refers to any marked position in the buffer
+/// - *Caret* refers to marks that represent concrete, user-controlled carets.
 ///   (i.e.: The places where text gets inserted)
-struct BufferRegions {
-    regions: HashMap<RegionId, Region>,
+struct BufferMarks {
+    marks: HashMap<MarkId, Mark>,
     /// All the active carets. There will always be at least one.
     /// The first element may be considered the "primary" caret,
     /// being the caret that will remain when exiting any sort of multi-caret mode.
     ///
-    /// All possible mutating interactions with [BufferRegions] must guarantee
-    /// that all ids stored here continue to actually map to a region.
-    carets: NonEmpty<RegionId>,
+    /// All possible mutating interactions with [BufferMarks] must guarantee
+    /// that all ids stored here continue to actually map to a mark.
+    carets: NonEmpty<MarkId>,
 }
 
-impl Default for BufferRegions {
+impl Default for BufferMarks {
     fn default() -> Self {
-        let primary_caret = Region::cursor(0);
-        let primary_caret_id = RegionId::gen();
-        let regions = maplit::hashmap! { primary_caret_id => primary_caret };
+        let primary_caret = Mark::sticky(0);
+        let primary_caret_id = MarkId::gen();
+        let marks = maplit::hashmap! { primary_caret_id => primary_caret };
         let carets = nonempty![primary_caret_id];
-        Self { regions, carets }
+        Self { marks, carets }
     }
 }
 
-impl BufferRegions {
+impl BufferMarks {
     fn apply_transformer<N: NodeInfo>(&mut self, trans: &mut Transformer<N>) {
-        for region in self.regions.values_mut() {
-            region.apply_transformer(trans, true);
+        for mark in self.marks.values_mut() {
+            mark.apply_transformer(trans);
         }
     }
 
@@ -48,20 +48,20 @@ impl BufferRegions {
         self.apply_transformer(&mut transformer);
     }
 
-    fn carets(&self) -> NonEmpty<Region> {
+    fn carets(&self) -> NonEmpty<Mark> {
         self.carets
             .iter()
-            .map(|x| *self.regions.get(x).expect("caret not found in regions"))
+            .map(|x| *self.marks.get(x).expect("caret not found in marks"))
             .collect::<Vec<_>>()
             .pipe(NonEmpty::from_vec)
             .unwrap()
     }
 
-    fn carets_mut(&mut self) -> impl Iterator<Item = &mut Region> {
+    fn carets_mut(&mut self) -> impl Iterator<Item = &mut Mark> {
         // TODO This is stupid, but iterating over self.carets instead and getting the refs
         // through get_mut doesn't work trivially, as rust can't verify that we won't get multiple
         // mut refs to the same entry as a result of overlapping keys...
-        self.regions
+        self.marks
             .iter_mut()
             .filter(|(k, _)| self.carets.contains(k))
             .map(|(_, v)| v)
@@ -72,7 +72,7 @@ pub struct Buffer {
     text: Rope,
     engine: Engine,
     undo_group_id: usize,
-    regions: BufferRegions,
+    marks: BufferMarks,
 }
 
 impl Buffer {
@@ -82,7 +82,7 @@ impl Buffer {
             undo_group_id: 1,
             engine: Engine::new(rope.clone()),
             text: rope,
-            regions: BufferRegions::default(),
+            marks: BufferMarks::default(),
         }
     }
 
@@ -90,10 +90,10 @@ impl Buffer {
         self.engine.get_head().to_string()
     }
 
-    pub fn all_carets(&self) -> NonEmpty<CursorPosition> {
-        self.regions
+    pub fn all_carets(&self) -> NonEmpty<Position> {
+        self.marks
             .carets()
-            .map(|x| CursorPosition::from_offset(&self.text, x.start))
+            .map(|x| Position::from_offset(&self.text, x.offset))
     }
 
     #[tracing::instrument(skip(self))]
@@ -102,7 +102,7 @@ impl Buffer {
         let undo_group = self.calculate_undo_group();
         //self.last_edit_type = self.this_edit_type;
 
-        self.regions.apply_delta(&delta);
+        self.marks.apply_delta(&delta);
         self.engine.edit_rev(1, undo_group, head_rev.token(), delta);
 
         self.text = self.engine.get_head().clone();
@@ -127,8 +127,8 @@ impl Buffer {
 
         let mut builder = DeltaBuilder::new(self.text.len());
         let text: Rope = chars.into();
-        for region in self.regions.carets() {
-            builder.replace(region, text.clone());
+        for mark in self.marks.carets() {
+            builder.replace(mark, text.clone());
         }
         let delta = builder.build();
         self.commit_delta(delta);
@@ -136,11 +136,11 @@ impl Buffer {
 
     fn delete_backward_at_carets(&mut self) {
         let mut builder = DeltaBuilder::new(self.text.len());
-        for region in self.regions.carets() {
+        for mark in self.marks.carets() {
             // See xi-editors `offset_for_delete_backwards` function in backward.rs...
             // all I'll say is `#[allow(clippy::cognitive_complexity)]`.
-            let delete_start = 1.max(region.start) - 1;
-            builder.delete(Region::new(delete_start, region.end));
+            let delete_start = 1.max(mark.offset) - 1;
+            builder.delete(delete_start..mark.offset);
         }
         let delta = builder.build();
         self.commit_delta(delta);
@@ -172,57 +172,56 @@ impl Buffer {
     }
 
     pub(crate) fn apply_movement_op(&mut self, op: MovementOp) {
-        for caret in self.regions.carets_mut() {
-            *caret = apply_movement_to_cursor(&self.text, *caret, op);
+        for mark in self.marks.carets_mut() {
+            *mark = apply_movement_to_mark(&self.text, *mark, op);
         }
     }
 }
 
-fn apply_movement_to_cursor(text: &Rope, region: Region, op: MovementOp) -> Region {
-    assert!(
-        region.is_cursor(),
-        "Movement for non-cursor regions is not implemented yet, and I'm not sure how to best approach this"
-    );
-    let cursor_offset = region.start;
+fn apply_movement_to_mark(text: &Rope, mark: Mark, op: MovementOp) -> Mark {
     let offset = match op {
         MovementOp::Left => text
-            .prev_grapheme_offset(cursor_offset)
-            .unwrap_or(cursor_offset),
+            .prev_grapheme_offset(mark.offset)
+            .unwrap_or(mark.offset),
         MovementOp::Right => text
-            .next_grapheme_offset(cursor_offset)
-            .unwrap_or(cursor_offset),
+            .next_grapheme_offset(mark.offset)
+            .unwrap_or(mark.offset),
         MovementOp::Up => {
-            let cursor_pos = CursorPosition::from_offset(text, cursor_offset);
-            if cursor_pos.line > 0 {
-                cursor_pos.with_line(cursor_pos.line - 1).to_offset(text)
+            let pos = Position::from_offset(text, mark.offset);
+            if pos.line > 0 {
+                pos.with_line(pos.line - 1).to_offset(text)
             } else {
-                cursor_offset
+                mark.offset
             }
         },
         MovementOp::Down => {
-            let cursor_pos = CursorPosition::from_offset(text, cursor_offset);
+            let pos = Position::from_offset(text, mark.offset);
             let last_line = text.line_of_offset(text.len());
-            if cursor_pos.line < last_line {
-                cursor_pos.with_line(cursor_pos.line + 1).to_offset(text)
+            if pos.line < last_line {
+                pos.with_line(pos.line + 1).to_offset(text)
             } else {
-                cursor_offset
+                mark.offset
             }
         },
     };
-    Region::cursor(offset)
+    Mark {
+        offset,
+        kind: mark.kind,
+    }
 }
 
+/// Position of a [Mark] in a [Buffer] by it's line and col.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct CursorPosition {
+pub struct Position {
     pub line: usize,
     pub col: usize,
 }
 
-impl CursorPosition {
+impl Position {
     fn from_offset(text: &Rope, offset: usize) -> Self {
         let line = text.line_of_offset(offset);
         let col = offset - text.offset_of_line(line);
-        CursorPosition { line, col }
+        Position { line, col }
     }
     /// Turn a position into an offset at that point,
     /// snapping to the end of the line if the cursors column is further than the line is long.
@@ -249,5 +248,37 @@ impl CursorPosition {
 
     pub fn with_col(self, col: usize) -> Self {
         Self { col, ..self }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_insert() {
+        let mut b = Buffer::open_ephemeral();
+        b.insert_at_carets("hel");
+        b.insert_at_carets("lo");
+        assert_eq!("hello", b.content_to_string());
+    }
+
+    #[test]
+    fn test_backspace() {
+        let mut b = Buffer::open_ephemeral();
+        b.insert_at_carets("a");
+        assert_eq!("a", b.content_to_string());
+        b.delete_backward_at_carets();
+        assert_eq!("", b.content_to_string());
+        b.delete_backward_at_carets();
+        assert_eq!("", b.content_to_string());
+    }
+
+    #[test]
+    fn test_move_caret_into_shorter_line() {
+        let mut b = Buffer::open_ephemeral();
+        b.insert_at_carets("hi\nworld");
+        b.apply_movement_op(MovementOp::Up);
+        b.insert_at_carets(",");
+        assert_eq!("hi,\nworld", b.content_to_string());
     }
 }
--- crates/bazed-core/src/lib.rs
+++ crates/bazed-core/src/lib.rs
@@ -3,6 +3,6 @@
 pub mod app;
 pub mod buffer;
 pub mod document;
-pub mod region;
-mod user_buffer_op;
 mod input_mapper;
+pub mod mark;
+mod user_buffer_op;
