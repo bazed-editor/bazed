3d6ee114db7d29629afbf02ce8e2633320054292
elkowar <5300871+elkowar@users.noreply.github.com>
2022-12-28 15:35:46 +0100

    refactor(buffer): move BufferRegions into separate module
    Also cleans up the BufferRegions code and adds a few tests

--- crates/bazed-core/src/region.rs
+++ crates/bazed-core/src/region.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 use uuid::Uuid;
 use xi_rope::interval::IntervalBounds;
 
@@ -47,6 +49,16 @@ impl Region {
         }
     }
 
+    /// Set the end (the head or the tail, depending on what offset is higher) to the given offset.
+    pub fn with_end_at(mut self, offset: usize) -> Self {
+        if self.head <= self.tail {
+            self.tail = offset;
+        } else {
+            self.head = offset;
+        }
+        self
+    }
+
     pub fn apply_transformer<N: xi_rope::tree::NodeInfo>(
         &mut self,
         transformer: &mut xi_rope::Transformer<N>,
@@ -56,18 +68,73 @@ impl Region {
     }
 
     /// Return the head and tail in order of offset
-    pub fn range(&self) -> (usize, usize) {
+    pub fn range(&self) -> Range<usize> {
         if self.head <= self.tail {
-            (self.head, self.tail)
+            self.head..self.tail
         } else {
-            (self.tail, self.head)
+            self.tail..self.head
         }
+    }
+
+    /// Check if this range overlaps with another range.
+    pub fn overlaps(&self, other: Self) -> bool {
+        let a_range = self.range();
+        let b_range = other.range();
+        a_range.start <= b_range.end && b_range.start <= a_range.end
+    }
+
+    /// Check if this region is before another one without any overlap.
+    pub fn is_strictly_before(&self, other: Self) -> bool {
+        self.head < other.head && !self.overlaps(other)
+    }
+
+    /// Merge this region into another region. Keeps all non-positional attributes of `self`.
+    /// Returns none when the regions did not overlap
+    pub fn merge(&self, other: Self) -> Option<Self> {
+        if !self.overlaps(other) {
+            return None;
+        }
+        let own_range = self.range();
+        let other_range = other.range();
+        Some(Self {
+            head: usize::min(own_range.start, other_range.start),
+            tail: usize::max(own_range.end, other_range.end),
+            ..*self
+        })
     }
 }
 
 impl IntervalBounds for Region {
     fn into_interval(self, _upper_bound: usize) -> xi_rope::Interval {
-        let (start, end) = self.range();
-        xi_rope::Interval { start, end }
+        self.range().into()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Region;
+
+    #[test]
+    fn test_overlaps() {
+        let check_works_both_ways = |a: Region, b: Region| {
+            assert!(a.overlaps(b), "A overlaps B");
+            assert!(b.overlaps(a), "B overlaps A");
+        };
+        check_works_both_ways(Region::sticky(10, 20), Region::sticky(15, 16));
+        check_works_both_ways(Region::sticky(10, 20), Region::sticky(5, 15));
+        check_works_both_ways(Region::sticky(10, 20), Region::sticky(15, 20));
+        check_works_both_ways(Region::sticky(10, 20), Region::sticky(15, 15));
+
+        assert!(Region::sticky(10, 20).overlaps(Region::sticky(20, 25)));
+        assert!(Region::sticky(20, 25).overlaps(Region::sticky(10, 20)));
+    }
+
+    #[test]
+    fn test_merge() {
+        assert_eq!(
+            Some(Region::sticky(10, 20)),
+            Region::sticky(10, 15).merge(Region::sticky(12, 20))
+        );
+        assert_eq!(None, Region::sticky(10, 15).merge(Region::sticky(18, 20)));
     }
 }
