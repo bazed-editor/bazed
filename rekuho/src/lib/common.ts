bf560b252d6d57327a1d39f7149000e3c3026d4a
cpli <dev@cpli.in>
2023-01-01 07:24:28 -0800

    feat(rekuho): send resizes, fullscreen editor

--- rekuho/src/lib/common.ts
+++ rekuho/src/lib/common.ts
@@ -1,13 +1,21 @@
-// const arr = [0, 0, 0]
-// arrayUpdate(arr, 1, x => x + 1)
-// arr => [0, 1, 0]
+/**
+ * ```
+ * const arr = [0, 0, 0]
+ * arrayUpdate(arr, 1, x => x + 1)
+ * arr => [0, 1, 0]
+ * ```
+ */
 export const arrayUpdate = <T>(arr: T[], n: number, f: (x: T) => T): void => {
   arr[n] = f(arr[n])
 }
 
-// const str = "abcccfg"
-// stringSplice("abcccfg", 4, "de", 2) => "cc"
-// str => "abcdefg"
+/**
+ * ```
+ * const str = "abcccfg"
+ * stringSplice("abcccfg", 4, "de", 2) => "cc"
+ * str => "abcdefg"
+ * ```
+ */
 export const stringSplice = (
   str: string,
   offset: number,
@@ -17,4 +25,51 @@ export const stringSplice = (
   const no = offset < 0 ? self.length + offset : offset
   const nod = no - deleteCount >= 0 ? no - deleteCount : no
   return str.substring(0, nod) + text + str.substring(no)
+}
+
+/** Allows to ensure exhaustive matches in switch-case statements */
+export const ensureExhaustive = (_: never) => {}
+
+/** elko really hates this name */
+export const elt = (
+  tag: string,
+  content?: string | HTMLElement[] | null,
+  className?: string | null,
+  cssText?: string,
+): HTMLElement => {
+  const element = document.createElement(tag)
+  if (className) {
+    element.className = className
+  }
+  if (cssText) {
+    element.style.cssText = cssText
+  }
+  if (typeof content === "string") {
+    element.appendChild(document.createTextNode(content))
+  } else if (Array.isArray(content)) {
+    for (let i = 0; i < content.length; ++i) {
+      element.appendChild(content[i])
+    }
+  }
+  return element
+}
+
+export const removeChildrenAndAdd = (
+  parent: HTMLElement,
+  element: HTMLElement | HTMLElement[],
+): HTMLElement => {
+  const removeChildren = (element: HTMLElement): HTMLElement => {
+    while (element.firstChild) {
+      element.removeChild(element.firstChild)
+    }
+    return element
+  }
+
+  if (Array.isArray(element)) {
+    removeChildren(parent)
+    element.forEach((child) => parent.appendChild(child))
+    return parent
+  } else {
+    return removeChildren(parent).appendChild(element)
+  }
 }
--- rekuho/src/lib/font.ts
+++ rekuho/src/lib/font.ts
@@ -1,18 +1,27 @@
-export const measure = (
-  font: string,
-): {
+import type { Font } from "./theme"
+
+export const fontToString = (font: Font): string => {
+  return `${font.weight} ${font.size} ${font.family}`
+}
+
+export type Metrics = {
   width: number | undefined
   actualHeight: number | undefined
   height: number | undefined
-} => {
+}
+
+/**
+ * using a canvas approach, measure width and height of monospace font characters
+ * @param font - in css format
+ * @returns calculated bounding box sizes
+ */
+export const measure = (font: string): Metrics => {
   const canvas = new OffscreenCanvas(0, 0)
   const context = canvas.getContext("2d") as OffscreenCanvasRenderingContext2D | null
   if (context) {
     context.font = font
   }
 
-  // FIXME: elkowar mentioned: "with a font of **ZERO WIDTH X-CHARACTERS**" this breaks.
-  // It does. ture. (typo intended)
   const metrics = context?.measureText("ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz")
 
   return {
@@ -27,4 +36,60 @@ export const measure = (
         ? metrics?.actualBoundingBoxAscent + metrics?.actualBoundingBoxDescent
         : undefined,
   }
+}
+
+import { elt, removeChildrenAndAdd } from "./common"
+
+/**
+ * using a dom-based aproach, measure width and height of monospace font characters
+ * @param element - to which to add child to measure
+ * @param font - in css format
+ * @returns size of box bounding some amount of text using `font`
+ */
+export const measureOnChild = (element: HTMLElement, font: string): Metrics => {
+  const ruler = element.appendChild(elt("div", null, "rekuho-measure"))
+  ruler.style.visibility = "hidden"
+  ruler.style.whiteSpace = "nowrap"
+  ruler.style.position = "absolute"
+
+  const width = computeAsciiWidth(ruler, font)
+  const height = computeLineHeight(ruler, font)
+  return { width, height, actualHeight: height }
+}
+
+/**
+ * using a `span` element measure monospace character width
+ * @param element - to which to add a span with multiple characters
+ * @param font - in css format
+ * @returns width of a single character in span of multiple characters of `font`
+ */
+const computeAsciiWidth = (element: HTMLElement, font: string): number => {
+  const anchor = elt("span", "x".repeat(10), null, `font: ${font}`)
+  const pre = elt("pre", [anchor])
+
+  removeChildrenAndAdd(element, pre)
+
+  const rect = anchor.getBoundingClientRect()
+  const width = (rect.right - rect.left) / 10
+  return width || 10
+}
+
+/**
+ * using a `pre` element measure height of a line
+ * @param element - to which to add a span with a character
+ * @param font - in css format
+ * @returns height of a line using a specific line
+ */
+const computeLineHeight = (element: HTMLElement, font: string): number => {
+  const pre = elt("pre", null, null, `font: ${font}`)
+  for (let i = 0; i < 49; ++i) {
+    pre.appendChild(document.createTextNode("x"))
+    pre.appendChild(elt("br"))
+  }
+  pre.appendChild(document.createTextNode("x"))
+
+  removeChildrenAndAdd(element, pre)
+
+  const height = pre.offsetHeight / 50
+  return height || 1
 }
