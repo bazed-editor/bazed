cc5d5fcdbd6c5ffd9061768b4afb74dfb2a88443
ElKowar <5300871+elkowar@users.noreply.github.com>
2023-01-01 16:48:20 +0100

    feat(rpc): properly handle pings

--- crates/bazed-rpc/src/server.rs
+++ crates/bazed-rpc/src/server.rs
@@ -30,42 +30,51 @@ pub async fn wait_for_client(
     let (mut ws_send, mut ws_recv) = ws_stream.split();
 
     let (mut to_backend_send, to_backend_recv) = futures::channel::mpsc::unbounded::<ToBackend>();
-
-    tokio::spawn(async move {
-        while let Some(msg) = ws_recv.next().await {
-            match msg {
-                Ok(tungstenite::Message::Text(json)) => {
-                    match serde_json::from_str::<ToBackend>(&json) {
-                        Ok(x) => {
-                            if let Err(err) = to_backend_send.send(x).await {
-                                tracing::warn!(
-                                    "Stopping ToBackend receiver forwarding loop: {err}"
-                                );
-                                break;
-                            }
-                        },
-                        Err(err) => {
-                            tracing::error!(raw_json = json, "Error parsing rpc message: {err:?}")
-                        },
-                    }
-                },
-                Ok(other) => tracing::warn!("Got unsupported websocket message: {other:?}"),
-                Err(err) => tracing::error!("Got error message from websocket: {err:?}"),
-            }
-        }
-    });
-
     let (to_frontend_send, mut to_frontend_recv) =
         futures::channel::mpsc::unbounded::<ToFrontend>();
 
     tokio::spawn(async move {
-        while let Some(msg) = to_frontend_recv.next().await {
-            tracing::debug!("Sending rpc call to client: {msg:?}");
-            let json = serde_json::to_string(&msg).unwrap();
-            if let Err(err) = ws_send.send(tungstenite::Message::Text(json)).await {
-                tracing::warn!("Stopping ToFrontend forwarding loop: {err}");
-                break;
-            }
+        loop {
+            tokio::select! {
+                msg = ws_recv.next() => {
+                    match msg {
+                        Some(Ok(tungstenite::Message::Text(json))) => {
+                            match serde_json::from_str::<ToBackend>(&json) {
+                                Ok(x) => {
+                                    if let Err(err) = to_backend_send.send(x).await {
+                                        tracing::warn!(
+                                            "Stopping ToBackend receiver forwarding loop: {err}"
+                                        );
+                                        break;
+                                    }
+                                },
+                                Err(err) => {
+                                    tracing::error!(raw_json = json, "Error parsing rpc message: {err:?}")
+                                },
+                            }
+                        },
+                        Some(Ok(tungstenite::Message::Ping(bytes))) => {
+                            if let Err(err) = ws_send.send(tungstenite::Message::Pong(bytes)).await {
+                                tracing::error!("Error sending pong: {err}");
+                                break;
+                            }
+                        },
+                        Some(Ok(other)) => tracing::warn!("Got unsupported websocket message: {other:?}"),
+                        Some(Err(err)) => tracing::error!("Got error message from websocket: {err:?}"),
+                        None => {
+                            break;
+                        }
+                    }
+                }
+                to_frontend_msg = to_frontend_recv.next() => {
+                    tracing::debug!("Sending rpc call to client: {to_frontend_msg:?}");
+                    let json = serde_json::to_string(&to_frontend_msg).unwrap();
+                    if let Err(err) = ws_send.send(tungstenite::Message::Text(json)).await {
+                        tracing::warn!("Stopping ToFrontend forwarding loop: {err}");
+                        break;
+                    }
+                }
+            };
         }
     });
 
