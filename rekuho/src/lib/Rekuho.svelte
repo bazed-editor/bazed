f4ada5352204d3ec044f267b5bfc3644567f378d
cpli <dev@cpli.in>
2023-01-06 03:05:08 -0800

    style(rekuho): enforce sass

--- rekuho/src/lib/Rekuho.svelte
+++ rekuho/src/lib/Rekuho.svelte
@@ -1,61 +1,48 @@
 <script lang="ts">
   import { example as configExample } from "./config"
-  import type { KeyInput, MouseWheel } from "./rpc"
-  import { initSession, Session } from "./rpc"
-  import type { CaretPosition } from "./core"
+  import { Session, initSession } from "./rpc"
   import { state } from "./core"
   import Portion from "./Portion.svelte"
+  import Welcome from "./Welcome.svelte"
 
   let session: Promise<Session> = initSession()
-
-  const onKeyInput = (session: Session, viewId: string, input: CustomEvent<KeyInput>) =>
-    session.handleKeyPressed(viewId, input.detail)
-
-  const onMouseDown = (session: Session, viewId: string, pos: CustomEvent<CaretPosition>) =>
-    session.handleMouseClicked(viewId, pos.detail)
-
-  const onMouseWheel = (session: Session, viewId: string, pos: CustomEvent<MouseWheel>) =>
-    session.handleMouseWheel(viewId, pos.detail.delta)
-
-  const onResize = (session: Session, viewId: string, pos: CustomEvent<[number, number]>) => {
-    session.handleUpdateView(
-      viewId,
-      {
-        width: pos.detail[0],
-        height: pos.detail[1],
-      }
-    )
-  }
-
-  // FIXME: CURRENT SVELTE PREPROCESSING CONFIGURATION DOES NOT PREPROCESS ATTRIBUTE CODE
-  const helperhelper = (session: Session, viewId: string) =>
-    <T>(callback: (session: Session, viewId: string, event: CustomEvent<T>) => void) =>
-    (event: CustomEvent<T>): void => callback(session, viewId, event)
 </script>
 
 <div class="rekuho">
   {#await session}
-    <div>Establishing session...</div>
+    <Welcome>
+      <div class="info">Establishing session...</div>
+    </Welcome>
   {:then session}
     {#each Object.entries($state.views) as [id, viewState], _}
-      {@const helper = helperhelper(session, id)}
       <Portion
         config={configExample}
         lines={viewState.lines}
         firstLine={viewState.firstLine}
         carets={viewState.carets}
-        on:keyinput={helper(onKeyInput)}
-        on:mousedown={helper(onMouseDown)}
-        on:mousewheel={helper(onMouseWheel)}
-        on:resize={helper(onResize)}
+        on:keyinput={(event) => session.handleKeyPressed(id, event.detail)}
+        on:mousedown={(event) => session.handleMouseClicked(id, event.detail)}
+        on:mousewheel={(event) => session.handleMouseWheel(id, event.detail)}
+        on:resize={(event) => session.handleUpdateView(id, event.detail)}
       />
     {/each}
+  {:catch}
+    <Welcome>
+      <div class="info failure">Failed establishing session.</div>
+    </Welcome>
   {/await}
 </div>
 
-<style>
-  .rekuho {
-    width: 100%;
-    height: 100%;
-  }
+<style lang="sass">
+  .rekuho
+    width: 100%
+    height: 100%
+    overflow: hidden
+
+  .info
+    font-size: 20px
+    text-align: center
+
+  .failure
+    color: crimson
 </style>
---
+++ rekuho/src/lib/Welcome.svelte
@@ -0,0 +1,20 @@
+<div class="container">
+  <div class="logo">BAZED</div>
+  <slot />
+</div>
+
+<style lang="sass">
+  .container 
+    width: 100vw
+    height: 100vh
+    display: flex
+    flex-direction: column
+    font-family: monospace
+
+  .logo
+    flex: 1
+    display: flex
+    justify-content: center
+    align-items: center
+    font-size: 400px
+</style>
