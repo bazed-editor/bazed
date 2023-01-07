3ba64a0f12ce274ec11565859d5548449cd64d6d
cpli <dev@cpli.in>
2023-01-07 09:16:01 -0800

    fix(bazed-tauri): allow setting `CARGO_NPM_COMMAND`

--- crates/bazed-tauri/build.rs
+++ crates/bazed-tauri/build.rs
@@ -1,5 +1,6 @@
 fn main() {
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let npm_command = std::env::var("CARGO_NPM_COMMAND").unwrap_or("npm".to_string());
     let svelte_dir = format!("{manifest_dir}/svelte");
 
     println!("cargo:rerun-if-changed={manifest_dir}/build.rs");
@@ -7,20 +8,23 @@ fn main() {
     println!("cargo:rerun-if-changed={svelte_dir}/package.json");
     println!("cargo:rerun-if-changed={svelte_dir}/svelte.config.js");
 
-    let status = std::process::Command::new("npm")
+    let status = std::process::Command::new(&npm_command)
         .args(&["--prefix", &svelte_dir, "ci"])
         .status()
         .expect("npm ci failed");
+
     if !status.success() {
         panic!("`npm ci` had non-zero exit status");
     }
 
-    let status = std::process::Command::new("npm")
+    let status = std::process::Command::new(&npm_command)
         .args(&["run", "--prefix", &svelte_dir, "build"])
         .status()
         .expect("npm run build failed");
+
     if !status.success() {
         panic!("`npm run build` had non-zero exit status");
     }
+
     tauri_build::build()
 }
