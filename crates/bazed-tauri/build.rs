15c35ef9e4f87e3c4baf61e2b619fd65db03a12d
elkowar <5300871+elkowar@users.noreply.github.com>
2023-01-07 12:11:30 +0100

    build(tauri): run npm ci and build in build.rs

--- crates/bazed-tauri/build.rs
+++ crates/bazed-tauri/build.rs
@@ -1,3 +1,26 @@
 fn main() {
+    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let svelte_dir = format!("{manifest_dir}/svelte");
+
+    println!("cargo:rerun-if-changed={manifest_dir}/build.rs");
+    println!("cargo:rerun-if-changed={svelte_dir}/src");
+    println!("cargo:rerun-if-changed={svelte_dir}/package.json");
+    println!("cargo:rerun-if-changed={svelte_dir}/svelte.config.js");
+
+    let status = std::process::Command::new("npm")
+        .args(&["--prefix", &svelte_dir, "ci"])
+        .status()
+        .expect("npm ci failed");
+    if !status.success() {
+        panic!("`npm ci` had non-zero exit status");
+    }
+
+    let status = std::process::Command::new("npm")
+        .args(&["run", "--prefix", &svelte_dir, "build"])
+        .status()
+        .expect("npm run build failed");
+    if !status.success() {
+        panic!("`npm run build` had non-zero exit status");
+    }
     tauri_build::build()
 }
