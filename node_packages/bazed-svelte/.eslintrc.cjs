dc1642f5038833318460a4c9678f63ecf808d932
elkowar <5300871+elkowar@users.noreply.github.com>
2023-01-11 21:24:52 +0100

    ci(svelte): configure and setup eslint

--- node_packages/bazed-svelte/.eslintrc.cjs
+++ node_packages/bazed-svelte/.eslintrc.cjs
@@ -17,4 +17,8 @@ module.exports = {
     es2017: true,
     node: true,
   },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-inferrable-types": "off"
+  }
 }
