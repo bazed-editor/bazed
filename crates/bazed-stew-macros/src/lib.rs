9487d2ae123941002000e2dd18652810d19b1360
elkowar <5300871+elkowar@users.noreply.github.com>
2023-03-04 18:57:23 +0100

    feat(stew): start implementing stew plugin macro

--- crates/bazed-stew-macros/src/lib.rs
+++ crates/bazed-stew-macros/src/lib.rs
@@ -3,8 +3,180 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Literal, Span};
 use proc_macro_error::abort;
-use quote::quote;
-use syn::{bracketed, parse::Parse, parse_macro_input, spanned::Spanned};
+use quote::{format_ident, quote};
+use syn::{bracketed, parse::Parse, parse_macro_input, spanned::Spanned, TraitItemMethod};
+
+#[proc_macro_attribute]
+#[proc_macro_error::proc_macro_error]
+pub fn plugin(_attrs: TokenStream, input: TokenStream) -> TokenStream {
+    let mut trayt = parse_macro_input!(input as syn::ItemTrait);
+
+    let functions = trayt
+        .items
+        .iter_mut()
+        .filter_map(|x| match x {
+            syn::TraitItem::Method(x) => Some(x),
+            _ => None,
+        })
+        .map(|x| {
+            match x.sig.output {
+                syn::ReturnType::Default => {}
+                syn::ReturnType::Type(_, ref mut x) => {
+                    *x = syn::parse_quote!(
+                        ::std::result::Result<#x, ::bazed_stew_interface::stew_rpc::Error>
+                    )
+                }
+            }
+            x
+        })
+        .collect::<Vec<_>>();
+
+    let trait_name = &trayt.ident;
+
+    let register_fns = functions
+        .iter()
+        .map(|x| make_register_fn(x))
+        .collect::<Vec<_>>();
+
+    let client_impl_fns = functions
+        .iter()
+        .enumerate()
+        .map(|(idx, x)| make_client_impl_fn(idx, x))
+        .collect::<Vec<_>>();
+
+    let client_get_fns = functions
+        .iter()
+        .map(|x| make_client_get_fn(x))
+        .collect::<Vec<_>>();
+
+    let client_impl_name = format_ident!("{}ClientImpl", trait_name);
+
+    quote! {
+        #trayt
+
+        pub use __internal::register_functions;
+        pub use __internal::#client_impl_name;
+
+        mod __internal {
+            use super::*;
+            use bazed_stew_interface::{
+                stew_rpc::{self, StewConnectionSender, StewConnectionReceiver, StewClient},
+                rpc_proto::{StewRpcCall, StewRpcMessage, FunctionId, PluginId}
+            };
+
+            pub struct #client_impl_name<S, D> {
+                client: StewClient<S, D>,
+                functions: Vec<FunctionId>,
+            }
+
+            impl <S, D> #client_impl_name<S, D> 
+            where 
+                S: StewConnectionSender<StewRpcCall> + Clone + 'static,
+                D: Send + Sync + 'static
+            {
+                pub async fn initialize(mut client: StewClient<S, D>, plugin_id: PluginId) -> Result<Self, stew_rpc::Error> {
+                    let mut functions = Vec::new();
+                    #(functions.push(#client_get_fns);)*
+                    Ok(Self {
+                        client,
+                        functions,
+                    })
+                }
+            }
+
+            #[::async_trait::async_trait]
+            impl <S, D> #trait_name for #client_impl_name<S, D> 
+            where 
+                S: StewConnectionSender<StewRpcCall> + Clone + 'static,
+                D: Send + Sync + 'static
+            {
+                #(#client_impl_fns)*
+            }
+
+            pub async fn register_functions<S, D>(client: &mut StewClient<S, D>) -> Result<(), stew_rpc::Error>
+            where
+                S: StewConnectionSender<StewRpcCall> + Clone + 'static,
+                D: #trait_name + Send + Sync + 'static
+            {
+                #(#register_fns)*
+                Ok(())
+            }
+        }
+    }
+    .into()
+}
+
+fn make_client_get_fn(function: &TraitItemMethod) -> proc_macro2::TokenStream {
+    let name = &function.sig.ident;
+    let name_str = syn::LitStr::new(&name.to_string(), name.span());
+    quote! {
+        client.get_fn(plugin_id, #name_str.to_string()).await?
+    }
+}
+
+fn make_client_impl_fn(n: usize, function: &TraitItemMethod) -> proc_macro2::TokenStream {
+    let args: Vec<_> = function
+        .sig
+        .inputs
+        .iter()
+        .filter_map(|x| match x {
+            syn::FnArg::Receiver(_) => None,
+            syn::FnArg::Typed(x) => Some(x),
+        })
+        .collect();
+    let arg_names: Vec<_> = args
+        .iter()
+        .map(|x| match &*x.pat {
+            syn::Pat::Ident(x) => Some(x),
+            _ => proc_macro_error::abort!(x.pat.span(), "Expected identifier"),
+        })
+        .collect();
+
+    let function_sig = &function.sig;
+
+    quote! {
+        #function_sig {
+            #[derive(serde::Serialize)]
+            struct Args { #(#args),* }
+            let args = Args { #(#arg_names),* };
+            self.client.call_fn_and_await_response(self.functions[#n], args).await
+        }
+    }
+}
+
+fn make_register_fn(function: &TraitItemMethod) -> proc_macro2::TokenStream {
+    let name = &function.sig.ident;
+    let name_str = syn::LitStr::new(&name.to_string(), name.span());
+    let args: Vec<_> = function
+        .sig
+        .inputs
+        .iter()
+        .filter_map(|x| match x {
+            syn::FnArg::Receiver(_) => None,
+            syn::FnArg::Typed(x) => Some(x),
+        })
+        .collect();
+    let arg_names: Vec<_> = args
+        .iter()
+        .map(|x| match &*x.pat {
+            syn::Pat::Ident(x) => Some(x),
+            _ => proc_macro_error::abort!(x.pat.span(), "Expected identifier"),
+        })
+        .collect();
+
+    quote! {{
+        #[derive(serde::Deserialize)]
+        struct Args { #(#args),* }
+        client.register_fn(#name_str, |userdata, args| Box::pin(async move {
+            let args: Args = serde_json::from_value(args).map_err(|e| serde_json::json!(e.to_string()))?;
+            let result = userdata.#name(#(args.#arg_names),*).await.unwrap();
+            match result {
+                Ok(x) => Ok(serde_json::to_value(x).unwrap()),
+                Err(x) => Err(serde_json::to_value(x).unwrap()),
+            }
+        })).await?;
+    }}
+}
 
 #[proc_macro]
 #[proc_macro_error::proc_macro_error]
