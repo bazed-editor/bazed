b67815441d62ea87958f368f0540aad6b968bb6a
elkowar <5300871+elkowar@users.noreply.github.com>
2023-03-05 13:08:43 +0100

    refactor(stew): cleanup procmacro implementation

--- crates/bazed-stew-macros/src/lib.rs
+++ crates/bazed-stew-macros/src/lib.rs
@@ -18,52 +18,27 @@ pub fn plugin(attrs: TokenStream, input: TokenStream) -> TokenStream {
         },
     };
 
-    let mut trayt = parse_macro_input!(input as syn::ItemTrait);
+    let trayt = parse_macro_input!(input as syn::ItemTrait);
 
     let functions = trayt
         .items
-        .iter_mut()
-        .filter_map(|x| match x {
-            syn::TraitItem::Method(x) => Some(x),
-            _ => None,
-        })
-        .map(|x| {
-            match x.sig.output {
-                syn::ReturnType::Default => {},
-                syn::ReturnType::Type(_, ref mut x) => {
-                    *x = syn::parse_quote!(
-                        ::std::result::Result<#x, ::bazed_stew_interface::stew_rpc::Error>
-                    )
-                },
-            }
-            x
+        .iter()
+        .map(|x| match x {
+            syn::TraitItem::Method(x) => {
+                if x.sig.asyncness.is_none() {
+                    abort!(x.span(), "All methods in a plugin must be async");
+                }
+                x
+            },
+            _ => proc_macro_error::abort!(x.span(), "Only methods are allowed in a plugin trait"),
         })
         .collect::<Vec<_>>();
 
     let trait_name = &trayt.ident;
 
-    let register_fns = functions
-        .iter()
-        .map(|x| make_register_fn(x))
-        .collect::<Vec<_>>();
-
-    let client_impl_fns = functions
-        .iter()
-        .enumerate()
-        .map(|(idx, x)| make_client_impl_fn(idx, x))
-        .collect::<Vec<_>>();
-
-    let client_get_fns = functions
-        .iter()
-        .map(|x| make_client_get_fn(x))
-        .collect::<Vec<_>>();
-
     let client_impl_name = format_ident!("{}ClientImpl", trait_name);
-
-    let plugin_version = args.version;
-    let stew_version_maj = args.stew_version.1;
-    let stew_version_min = args.stew_version.2;
-    let plugin_name = args.name;
+    let client_impl = make_client_impl(&args, &client_impl_name, &functions);
+    let server_module = make_server_module(&args, trait_name, &functions);
 
     quote! {
         #trayt
@@ -78,72 +53,117 @@ pub fn plugin(attrs: TokenStream, input: TokenStream) -> TokenStream {
                 rpc_proto::{StewRpcCall, StewRpcMessage, FunctionId, PluginId, PluginMetadata},
                 semver,
             };
+            #server_module
+            #client_impl
 
-            pub struct #client_impl_name<S, D> {
-                client: StewClient<S, D>,
-                functions: Vec<FunctionId>,
-            }
-
-            impl <S, D> #client_impl_name<S, D>
-            where
-                S: StewConnectionSender<StewRpcCall> + Clone + 'static,
-                D: Send + Sync + 'static
-            {
-                pub async fn load(mut client: StewClient<S, D>) -> Result<Self, stew_rpc::Error> {
-                    Self::load_at(client, #plugin_version.parse().unwrap())
-                        .await
-                }
-
-                pub async fn load_at(mut client: StewClient<S, D>, version: semver::VersionReq) -> Result<Self, stew_rpc::Error> {
-                    let plugin_info = client
-                        .load_plugin(#plugin_name.to_string(), version)
-                        .await?;
-                    Self::initialize(client, plugin_info.plugin_id).await
-                }
-
-                pub async fn initialize(mut client: StewClient<S, D>, plugin_id: PluginId) -> Result<Self, stew_rpc::Error> {
-                    let mut functions = Vec::new();
-                    #(functions.push(#client_get_fns);)*
-                    Ok(Self {
-                        client,
-                        functions,
-                    })
-                }
-            }
-
-            #[::async_trait::async_trait]
-            impl <S, D> #trait_name for #client_impl_name<S, D>
-            where
-                S: StewConnectionSender<StewRpcCall> + Clone + 'static,
-                D: Send + Sync + 'static
-            {
-                #(#client_impl_fns)*
-            }
-
-
-            pub mod server {
-                use super::*;
-                pub async fn initialize<S, D>(client: &mut StewClient<S, D>) -> Result<(), stew_rpc::Error>
-                where
-                    S: StewConnectionSender<StewRpcCall> + Clone + 'static,
-                    D: #trait_name + Send + Sync + 'static
-                {
-                    client
-                        .send_call(StewRpcCall::Metadata(PluginMetadata {
-                            api_major: #stew_version_maj,
-                            api_minor: #stew_version_min,
-                            name: #plugin_name.to_string(),
-                            version: #plugin_version.parse().unwrap(),
-                        }))
-                        .await?;
-
-                    #(#register_fns)*
-                    Ok(())
-                }
-            }
         }
     }
     .into()
+}
+
+fn make_metadata_struct_instance(args: &PluginAttr) -> proc_macro2::TokenStream {
+    let plugin_version = &args.version;
+    let stew_version_maj = args.stew_version.1;
+    let stew_version_min = args.stew_version.2;
+    let plugin_name = &args.name;
+    quote! {
+        PluginMetadata {
+            api_major: #stew_version_maj,
+            api_minor: #stew_version_min,
+            name: #plugin_name.to_string(),
+            version: #plugin_version.parse().unwrap(),
+        }
+    }
+}
+
+fn make_server_module(
+    args: &PluginAttr,
+    trait_name: &syn::Ident,
+    functions: &[&TraitItemMethod],
+) -> proc_macro2::TokenStream {
+    let register_fns = functions.iter().map(|x| make_register_fn(x));
+    let metadata = make_metadata_struct_instance(args);
+    quote! {
+        pub mod server {
+            use super::*;
+            pub async fn initialize<S, D>(client: &mut StewClient<S, D>) -> Result<(), stew_rpc::Error>
+            where
+                S: StewConnectionSender<StewRpcCall> + Clone + 'static,
+                D: #trait_name + Send + Sync + 'static
+            {
+                client.send_call(StewRpcCall::Metadata(#metadata)).await?;
+                #(#register_fns)*
+                Ok(())
+            }
+        }
+    }
+}
+
+fn make_client_impl(
+    args: &PluginAttr,
+    client_impl_name: &syn::Ident,
+    functions: &[&syn::TraitItemMethod],
+) -> proc_macro2::TokenStream {
+    let client_impl_fns = functions
+        .iter()
+        .enumerate()
+        .map(|(idx, x)| make_client_impl_fn(idx, (*x).clone()));
+
+    let plugin_version = &args.version;
+    let plugin_name = &args.name;
+
+    let client_get_fns = functions.iter().map(|function| {
+        let name = &function.sig.ident;
+        let name_str = syn::LitStr::new(&name.to_string(), name.span());
+        quote!(client.get_fn(plugin_id, #name_str.to_string()).await?)
+    });
+
+    quote! {
+        pub struct #client_impl_name<S, D> {
+            client: StewClient<S, D>,
+            functions: Vec<FunctionId>,
+        }
+
+        impl <S, D> #client_impl_name<S, D>
+        where
+            S: StewConnectionSender<StewRpcCall> + Clone + 'static,
+            D: Send + Sync + 'static
+        {
+            pub async fn load(mut client: StewClient<S, D>) -> Result<Self, stew_rpc::Error> {
+                Self::load_at(client, #plugin_version.parse().unwrap())
+                    .await
+            }
+
+            pub async fn load_at(mut client: StewClient<S, D>, version: semver::VersionReq) -> Result<Self, stew_rpc::Error> {
+                let plugin_info = client
+                    .load_plugin(#plugin_name.to_string(), version)
+                    .await?;
+                Self::initialize(client, plugin_info.plugin_id).await
+            }
+
+            pub async fn initialize(mut client: StewClient<S, D>, plugin_id: PluginId) -> Result<Self, stew_rpc::Error> {
+                let functions = vec![ #(#client_get_fns),* ];
+                Ok(Self { client, functions })
+            }
+
+            #(#client_impl_fns)*
+        }
+
+    }
+}
+
+fn wrap_return_type(
+    mut sig: syn::Signature,
+    wrap: impl FnOnce(Box<syn::Type>) -> syn::Type,
+) -> syn::Signature {
+    sig.output = match sig.output {
+        syn::ReturnType::Default => syn::ReturnType::Type(
+            syn::Token![->](Span::call_site()),
+            Box::new(wrap(syn::parse_quote!(()))),
+        ),
+        syn::ReturnType::Type(tok, old) => syn::ReturnType::Type(tok, Box::new(wrap(old))),
+    };
+    sig
 }
 
 #[derive(darling::FromMeta)]
@@ -153,42 +173,41 @@ struct PluginAttr {
     stew_version: Version,
 }
 
-fn make_client_get_fn(function: &TraitItemMethod) -> proc_macro2::TokenStream {
-    let name = &function.sig.ident;
-    let name_str = syn::LitStr::new(&name.to_string(), name.span());
-    quote! {
-        client.get_fn(plugin_id, #name_str.to_string()).await?
-    }
-}
+fn make_client_impl_fn(n: usize, mut function: TraitItemMethod) -> proc_macro2::TokenStream {
+    // we have to get this before changing the signature
+    let returns_result = returns_result(&function.sig);
 
-fn make_client_impl_fn(n: usize, function: &TraitItemMethod) -> proc_macro2::TokenStream {
-    let args: Vec<_> = function
-        .sig
-        .inputs
+    function.sig = wrap_return_type(
+        function.sig,
+        |old| syn::parse_quote!(::std::result::Result<#old, stew_rpc::Error>),
+    );
+    let inputs = &function.sig.inputs;
+    let args: Vec<_> = inputs
         .iter()
         .filter_map(|x| match x {
             syn::FnArg::Receiver(_) => None,
             syn::FnArg::Typed(x) => Some(x),
         })
         .collect();
-    let arg_names: Vec<_> = args
-        .iter()
-        .map(|x| match &*x.pat {
-            syn::Pat::Ident(x) => Some(x),
-            _ => proc_macro_error::abort!(x.pat.span(), "Expected identifier"),
-        })
-        .collect();
+
+    let arg_names = args.iter().map(|x| match &*x.pat {
+        syn::Pat::Ident(x) => Some(x),
+        _ => proc_macro_error::abort!(x.pat.span(), "Expected identifier"),
+    });
 
     let function_sig = &function.sig;
 
-    let call_fn_line = if returns_result(&function.sig) {
+    let call_fn_line = if returns_result {
         quote! { self.client.call_fn_and_await_response(self.functions[#n], args).await }
     } else {
         quote! { self.client.call_fn_and_await_response_infallible(self.functions[#n], args).await }
     };
 
+    let attrs = &function.attrs;
     quote! {
-        #function_sig {
+        #[tracing::instrument(skip(self))]
+        #(#attrs)*
+        pub #function_sig {
             #[derive(serde::Serialize)]
             struct Args { #(#args),* }
             let args = Args { #(#arg_names),* };
@@ -249,7 +268,7 @@ fn make_register_fn(function: &TraitItemMethod) -> proc_macro2::TokenStream {
 
         client.register_fn(#name_str, |userdata, args| Box::pin(async move {
             let args: Args = serde_json::from_value(args).map_err(|e| serde_json::json!(e.to_string()))?;
-            let result = userdata.#name(#(args.#arg_names),*).await.unwrap();
+            let result = userdata.#name(#(args.#arg_names),*).await;
             #return_code
         })).await?;
     }}
@@ -269,21 +288,16 @@ impl darling::FromMeta for Version {
     fn from_value(version: &syn::Lit) -> darling::Result<Self> {
         let version = match version {
             syn::Lit::Str(x) => x,
-            _ => abort!(version, "Version must be formatted as \"<major>.<minor>\""),
+            _ => abort!(version, "Version must be formatted as \"<major>.<minor>\"."),
         };
-
         if let Some((major, minor)) = version.value().split_once('.') {
             match (major.parse(), minor.parse()) {
                 (Ok(major), Ok(minor)) => Ok(Version(version.span(), major, minor)),
-                (Err(_), _) => {
-                    abort!(version, "Malformed major version, must be an integer")
-                },
-                (_, Err(_)) => {
-                    abort!(version, "Malformed minor version, must be an integer")
-                },
+                (Err(_), _) => abort!(version, "Malformed major version, must be an integer"),
+                (_, Err(_)) => abort!(version, "Malformed minor version, must be an integer"),
             }
         } else {
-            abort!(version, "Version must be formatted as <major>.<minor>.");
+            abort!(version, "Version must be formatted as \"<major>.<minor>\".");
         }
     }
 }
